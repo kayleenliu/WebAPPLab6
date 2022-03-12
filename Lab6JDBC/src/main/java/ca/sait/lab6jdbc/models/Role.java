e02d2db5461509c58a32544873e6abcfc0fcf705
Kayleen Liu <gaoliu216@hotmail.com>
2022-03-11 19:55:54 -0700

    finished constructor and dataaccess package

---
+++ Lab6JDBC/src/main/java/ca/sait/lab6jdbc/models/Role.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sait.lab6jdbc.models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Kayleen
+ */
+public class Role implements Serializable {
+    private int id;
+    private String name;
+    
+    public Role(){
+        
+    }
+    
+    public Role(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
---
+++ Lab6JDBC/src/main/java/ca/sait/lab6jdbc/models/User.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sait.lab6jdbc.models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Kayleen
+ */
+public class User implements Serializable {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private boolean active;
+    private Role role;
+    public User(){
+        
+    }
+    
+    public User(String email, boolean active, String firstName, String lastName, String password, Role role){
+        this.email = email;
+        this.active = active;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    
+    
+    
+}
